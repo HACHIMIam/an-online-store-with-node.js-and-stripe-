afef8e9aa606e60ad8cf18ae7beaf501e88438df
hachimi ahmed amine <hachimiaminne@gmail.com>
2017-05-16 03:04:10 +0100

    set up sign up and sign in with passport local strategy

--- populateProducts.js
+++ populateProducts.js
@@ -1,45 +1,38 @@
-var Product = require('./models/product');
+var Product = require('../models/product');
 
 var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
 
-mongoose.connect('localhost:27017/mystore');
+mongoose.connect('localhost:27017/shopping');
 
 var products = [
     new Product({
-        imagePath: 'http://placehold.it/350x350',
-        title: 'lorme ipsum',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A qui minus ipsam maiores magnam blanditiis ipsum omnis architecto, neque corporis soluta possimus hic inventore amet fugit doloribus vel sunt quasi.',
+        imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+        title: 'Gothic Video Game',
+        description: 'Awesome Game!!!!',
         price: 10
     }),
     new Product({
-        imagePath: 'http://placehold.it/350x350',
-        title: 'lorem ipsum',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A qui minus ipsam maiores magnam blanditiis ipsum omnis architecto, neque corporis soluta possimus hic inventore amet fugit doloribus vel sunt quasi.',
+        imagePath: 'http://eu.blizzard.com/static/_images/games/wow/wallpapers/wall2/wall2-1440x900.jpg',
+        title: 'World of Warcraft Video Game',
+        description: 'Also awesome? But of course it was better in vanilla ...',
         price: 20
     }),
     new Product({
-        imagePath: 'http://placehold.it/350x350',
-        title: 'lorem ipsum',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A qui minus ipsam maiores magnam blanditiis ipsum omnis architecto, neque corporis soluta possimus hic inventore amet fugit doloribus vel sunt quasi. ',
+        imagePath: 'https://support.activision.com/servlet/servlet.FileDownload?file=00PU000000Rq6tz',
+        title: 'Call of Duty Video Game',
+        description: 'Meh ... nah, it\'s okay I guess',
         price: 40
     }),
     new Product({
-        imagePath: 'http://placehold.it/350x350',
-        title: 'lorem ipsum',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A qui minus ipsam maiores magnam blanditiis ipsum omnis architecto, neque corporis soluta possimus hic inventore amet fugit doloribus vel sunt quasi.',
+        imagePath: 'https://pmcdeadline2.files.wordpress.com/2014/02/minecraft__140227211000.jpg',
+        title: 'Minecraft Video Game',
+        description: 'Now that is super awesome!',
         price: 15
     }),
     new Product({
-        imagePath: 'http://placehold.it/350x350',
-        title: 'lorem ipsum',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A qui minus ipsam maiores magnam blanditiis ipsum omnis architecto, neque corporis soluta possimus hic inventore amet fugit doloribus vel sunt quasi.',
-        price: 50
-    }),
-    new Product({
-        imagePath: 'http://placehold.it/350x350',
-        title: 'lorem ipsum',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A qui minus ipsam maiores magnam blanditiis ipsum omnis architecto, neque corporis soluta possimus hic inventore amet fugit doloribus vel sunt quasi.',
+        imagePath: 'https://d1r7xvmnymv7kg.cloudfront.net/sites_products/darksouls3/assets/img/DARKSOUL_facebook_mini.jpg',
+        title: 'Dark Souls 3 Video Game',
+        description: 'I died!',
         price: 50
     })
 ];
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
+
 var Product = require('../models/product');
 
-
 /* GET home page. */
-
 router.get('/', function(req, res, next) {
     Product.find(function(err, docs) {
         var productChunks = [];
@@ -12,8 +11,8 @@ router.get('/', function(req, res, next) {
         for (var i = 0; i < docs.length; i += chunkSize) {
             productChunks.push(docs.slice(i, i + chunkSize));
         }
-        res.render('index', { title: 'online store', products: productChunks });
-    });
+        res.render('index', { title: 'online-shop', products: productChunks });
+    })
 });
 
 module.exports = router;
